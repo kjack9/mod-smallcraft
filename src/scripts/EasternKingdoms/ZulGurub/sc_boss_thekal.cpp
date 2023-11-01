3ef79d2e8d21b6758270082838c9b3238d52289e
KJack <kgithub@kellerg.com>
2023-11-01 00:02:57 -0400

    ZG: Thekal (Tiger)

---
+++ src/scripts/EasternKingdoms/ZulGurub/sc_boss_thekal.cpp
@@ -0,0 +1,34 @@
+#include "GameObjectAI.h"
+#include "MoveSplineInit.h"
+#include "ScriptMgr.h"
+#include "ScriptedCreature.h"
+#include "Smallcraft.h"
+#include "SmartAI.h"
+#include "SpellInfo.h"
+#include "SpellScript.h"
+#include "TaskScheduler.h"
+#include "../../src/server/scripts/EasternKingdoms/ZulGurub/zulgurub.h"
+
+namespace ac_thekal
+{
+    #include "../../src/server/scripts/EasternKingdoms/ZulGurub/boss_thekal.cpp"
+}
+
+class sc_boss_thekal_DatabaseScript : public DatabaseScript
+{
+public:
+    sc_boss_thekal_DatabaseScript() : DatabaseScript("sc_boss_thekal_DatabaseScript") { }
+
+    void OnAfterDatabaseLoadCreatureTemplates(std::vector<CreatureTemplate*> creatureTemplates) override
+    {
+        // Zealot Zath (11348) - Tiger Boss Add
+        // make kite-able
+        creatureTemplates[11348]->MechanicImmuneMask = 536936977; // can't be CC'd, but can be slowed/distracted/rooted/etc
+    }
+
+};
+
+void load_sc_boss_thekal()
+{
+    new sc_boss_thekal_DatabaseScript();
+}
--- src/scripts/EasternKingdoms/ZulGurub/sc_instance_zulgurub.cpp
+++ src/scripts/EasternKingdoms/ZulGurub/sc_instance_zulgurub.cpp
@@ -1,43 +1,10 @@
 #include "ScriptMgr.h"
 #include "SpellInfo.h"
 #include "sc_boss_jeklik.cpp"
-
-class sc_instance_zulgurub_GlobalScript : public GlobalScript
-{
-public:
-    sc_instance_zulgurub_GlobalScript() : GlobalScript("sc_instance_zulgurub_GlobalScript") { }
-
-    void OnLoadSpellCustomAttr(SpellInfo* spellInfo) override
-    {
-        switch (spellInfo->Id)
-        {
-            // Jeklik - Bat Aspect Boss
-            // Blaze (23972)
-            case (23972):
-                LOG_DEBUG("module.Smallcraft", "sc::sc_instance_zulgurub_GlobalScript:: Correcting spell {} ({})...", spellInfo->Id, spellInfo->SpellName[0]);
-                spellInfo->AttributesEx2 |= SPELL_ATTR2_CANT_CRIT;
-                break;
-            default:
-                break;
-        }
-    }
-};
-
-class sc_instance_zulgurub_DatabaseScript : public DatabaseScript
-{
-public:
-    sc_instance_zulgurub_DatabaseScript() : DatabaseScript("sc_instance_zulgurub_DatabaseScript") { }
-
-    void OnAfterDatabaseLoadCreatureTemplates(std::vector<CreatureTemplate*> creatureTemplates) override
-    {
-        // Zealot Zath (11348) - Tiger Boss Add
-        // make kite-able
-        creatureTemplates[11348]->MechanicImmuneMask = 536936977; // can't be CC'd, but can be slowed/distracted/rooted/etc
-    }
-
-};
+#include "sc_boss_thekal.cpp"
 
 void load_sc_instance_zulgurub()
 {
     load_sc_boss_jeklik();
+    load_sc_boss_thekal();
 }
