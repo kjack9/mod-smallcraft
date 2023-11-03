a284746405884b0966140899f5bceec55e674f51
KJack <kgithub@kellerg.com>
2023-11-03 02:34:03 -0400

    Fix SQL

--- data/sql/db-characters/base/SmallCraft_TempSpells.sql
+++ data/sql/db-characters/base/SmallCraft_TempSpells.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `custom_smallcraft_tempspells` (
 	`GUID` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Player\'s GUID',
 	`SpellID` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Granted SpellID',
 	PRIMARY KEY (`ID`) USING BTREE,
-	INDEX `PlayerGUID` (`PlayerGUID`) USING BTREE
+	INDEX `GUID` (`GUID`) USING BTREE
 )
 COMMENT='Stores any temporary spells that have been granted to characters by the Smallcraft TempSpells feature.'
 COLLATE='utf8mb4_unicode_ci'
